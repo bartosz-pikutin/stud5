3cb5078c8c77e8a044b8a5039cf7008b9a6bc756
bartosz-pikutin <s24819@pjwstk.edu.pl>
2024-03-11 00:42:22 +0100

    Modyfikacja 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program 
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Jak masz na imie?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Cześć {name}");
+
+        }   
+    }
+}
+
+
+//Console.WriteLine("Hello, World!");
